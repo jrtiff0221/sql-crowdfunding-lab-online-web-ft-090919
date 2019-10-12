7a90bb2d6db77ff8393d6be5f1a4bf06870a9c6f
Johnnae Tiffany <jrtiff0221@gmail.com>
2019-10-12 21:03:27 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -32,3 +32,12 @@ INSERT INTO projects(title,category,funding_goal,start_date,end_date) VALUES
 ("quiditch", "school", 800, 2019,2022),
 ("other", "school", 300, 2020, 2021);
 
+INSERT INTO pledges(amount,user_id,project_id) VALUES
+(500,1,1),
+(400,1,2),
+(500,1,1),
+(400,1,2),
+(500,1,1),
+(400,1,2),
+(500,1,1),
+(400,1,2),
