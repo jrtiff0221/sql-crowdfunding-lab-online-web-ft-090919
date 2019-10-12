215f332a91b56c90e79bd0a6861785f879c4c9c5
Johnnae Tiffany <jrtiff0221@gmail.com>
2019-10-12 20:46:14 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,22 @@
-IN
+INSERT INTO users(name,age) VALUES 
+("Sirus Black", 40),
+("Harry Potter", 15),
+("Ron Weasley", 15),
+("Draco Malfoy", 16),
+("Hermione Granger", 14),
+("Uncle Vernon", 42),
+("Lord Voldermort", 58),
+("Dumbledore", 102),
+("George Weasley", 17),
+("Dobby", 56),
+("Ginny Weasley", 14),
+("Neville Longbottom", 15),
+("Luna Lovegood", 13),
+("Bellatrix", 53),
+("Hagrid", 57),
+("Snape" ,54),
+("Nymphadora", 34),
+("Dolores", 72),
+("Peter Petigrue", 43)
+("James Potter", 50)
+
